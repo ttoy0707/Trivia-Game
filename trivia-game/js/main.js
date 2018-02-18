c7b54a0fabd860182811426c16f779987ef95d90
ttoy0707 <34200642+ttoy0707@users.noreply.github.com>
2018-02-17 20:40:23 -0600

    Add files via upload

---
+++ trivia-game/js/main.js
@@ -0,0 +1,120 @@
+var startScreen;
+var gameHTML;
+var counter = 30;
+var questionArray = ["What is the capital of Missouri?", "What is the capital of California?", "What is the capital of Kansas?", "What is the capital of Texas", "What is the capital of Florida?", "What is the capital of Washington?", "What is the capital of Oklahoma?", "What is the capital of Colorado?"];
+var answerArray = [["Jefferson City", "St. Louis", "Kansas City", "Branson"], ["Los Angeles","Sacramento","San Diego","San Fransisco"], ["Lawrence", "Olethe", "Topeka", "Kansas City"], ["Houston","Dallas","Austin","San Antonio"], ["Tampa", "Orlando", "Miami", "Tallahassee"], ["Olympia","Seattle","Spokane","Tacoma"], ["Tulsa", "Oklahoma City", "Norman", "Edmond"], ["Colorado Springs","Boulder","Aspen","Denver"]];
+var correctAnswers = ["A. Jefferson City", "B. Sacramento", "C. Topeka", "C. Austin", "D. Tallahassee", "A. Olympia", "B. Oklahoma City", "D. Denver"];
+var questionCounter = 0;
+var selecterAnswer;
+var theClock;
+var correctTally = 0;
+var incorrectTally = 0;
+var unansweredTally = 0;
+
+
+$(document).ready(function() {
+
+function initialScreen() {
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+	$(".mainArea").html(startScreen);
+}
+
+initialScreen();
+
+$("body").on("click", ".start-button", function(event){
+	event.preventDefault();  
+	generateHTML();
+
+	timerWrapper();
+
+}); 
+
+$("body").on("click", ".answer", function(event){
+	
+	selectedAnswer = $(this).text();
+	if(selectedAnswer === correctAnswers[questionCounter]) {
+		
+
+		clearInterval(theClock);
+		generateWin();
+	}
+	else {
+		
+		clearInterval(theClock);
+		generateLoss();
+	}
+}); 
+$("body").on("click", ".reset-button", function(event){
+	resetGame();
+}); 
+
+});  
+
+function generateLossDueToTimeOut() {
+	unansweredTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 2000); 
+}
+
+function generateWin() {
+	correctTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/correct.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 2000);  
+}
+
+function generateLoss() {
+	incorrectTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 2000); 
+}
+
+function generateHTML() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function wait() {
+	if (questionCounter < 7) {
+	questionCounter++;
+	generateHTML();
+	counter = 30;
+	timerWrapper();
+	}
+	else {
+		finalScreen();
+	}
+}
+
+function timerWrapper() {
+	theClock = setInterval(thirtySeconds, 1000);
+	function thirtySeconds() {
+		if (counter === 0) {
+			clearInterval(theClock);
+			generateLossDueToTimeOut();
+		}
+		if (counter > 0) {
+			counter--;
+		}
+		$(".timer").html(counter);
+	}
+}
+
+function finalScreen() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function resetGame() {
+	questionCounter = 0;
+	correctTally = 0;
+	incorrectTally = 0;
+	unansweredTally = 0;
+	counter = 30;
+	generateHTML();
+	timerWrapper();
+}
+
+
